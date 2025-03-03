8b79f33d68ed6ab8e00a00a98dc7c8b573a8ddb1
Yehor Demenchuk <yehor.demenchuk@student.tuke.sk>
2025-03-03 12:53:00 +0100

    fix bugs

--- src/main/java/org/bankapi/repository/AccountRepository.java
+++ src/main/java/org/bankapi/repository/AccountRepository.java
@@ -2,6 +2,8 @@ package org.bankapi.repository;
 
 import org.bankapi.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 }
